78c235c08e500dba8094113864a998285e48a215
Sav1taRG <52829649+Sav1taRG@users.noreply.github.com>
2023-05-10 16:26:56 +0200

    Initial Commit

---
+++ VirusUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirusUIKit
+//
+//  Created by Roman Golubinko on 10.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
